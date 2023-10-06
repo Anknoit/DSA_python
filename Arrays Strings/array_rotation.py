6550c0798279f706dd7f03008b7a44d52d7566d7
Anknoit <ankitjha8891@gmail.com>
2023-10-06 20:29:31 +0530

    added error log for excel

--- Arrays Strings/array_rotation.py
+++ Arrays Strings/array_rotation.py
@@ -1,7 +1,9 @@
 T = int(input())
 for i in range(T):
-    N, K = map(int, input().split())
-    arr = list(input().split())
+    N, K = map(
+        int, input().split()
+    )  # N= No. of elements for the array, K = index position from where array will be rotated
+    arr = list(input().split())  # array of N elemets
     print(type(N), type(K), type(arr))
     # print(len(arr))
     if len(arr) <= N:
--- Arrays Strings/string_reverse.py
+++ Arrays Strings/string_reverse.py
@@ -1,7 +1,7 @@
 def arr_rev(N):
     newArr = []
-    for i in range((len(N) - 1), -1, -1):
-        newArr += N[i]
+    for i in range((len(N) - 1), -1, -1):  # Backward loop
+        newArr += N[i]  # appending/adding elements of N backwards using backward loop
     # print(newArr)
     print(" ".join(newArr))
     return " ".join(newArr)
